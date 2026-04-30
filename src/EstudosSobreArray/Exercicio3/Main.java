e6993f74c0aed58a57e0539cee1c0803d79a604d
Gabriel Munhoz <gabrielmunhoz21@gmail.com>
2026-04-30 14:19:44 -0300

    Exercício 3

---
+++ src/EstudosSobreArray/Exercicio3/Main.java
@@ -0,0 +1,59 @@
+package EstudosSobreArray.Exercicio3;
+
+// Exercício 3 — Busca simples
+// Peça para o usuário digitar um nome de livro e:
+// Verifique se ele existe no array
+// Retorne a posição ou "não encontrado"
+//👉 Dica: usar equals() (não use ==).
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void imprimirLista(String[] livros){
+
+        int contagem = 1;
+        for (String livro : livros){
+            System.out.println(contagem + "- " + livro);
+            contagem++;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner inserir = new Scanner(System.in);
+
+        String[] livros = {
+                "Dom Casmurro",
+                "1984",
+                "O Hobbit",
+                "A Revolução dos Bichos",
+                "Harry Potter",
+                "O Senhor dos Aneis",
+                "Percy Jackson",
+                "O Pequeno Príncipe",
+                "Crepusculo",
+                "Jogos Vorazes"
+        };
+
+        imprimirLista(livros);
+
+        System.out.println("\nBuscar livro: ");
+        String buscar = inserir.nextLine();
+        boolean encontrado = false;
+
+        for (int i = 0; i < livros.length; i ++){
+            if (livros[i].equalsIgnoreCase(buscar)){
+                System.out.println("Livro \"" + buscar + "\" encontrado na posição " + (i+1));
+                encontrado = true;
+                break;
+            }
+        }
+
+        if (!encontrado){
+            System.out.println("Livro \"" + buscar + "\" não encontrado.");
+        }
+
+        inserir.close();
+    }
+}
