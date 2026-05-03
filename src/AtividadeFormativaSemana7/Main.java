3f9e8be887c9366d29ddcd469c59ad12920fdc69
Gabriel Munhoz <gabrielmunhoz21@gmail.com>
2026-05-03 14:04:13 -0300

    DFS e BFS fornecidos pelo professor

--- src/AtividadeFormativaSemana7/Main.java
+++ src/AtividadeFormativaSemana7/Main.java
@@ -1,5 +1,50 @@
 package AtividadeFormativaSemana7;
 
+
+/*
+
+1- digitar nome de um livro
+2- procurar livro na arvore binária
+3- mostrar todos os livros que o BFS e o DFS percorreram até encontrar o livro desejado
+
+*/
+
+import java.util.Queue;
+import java.util.LinkedList;
+
+class DFS {
+    public static void percorrerArvore(No no) {
+        if (no == null) {
+            return;
+        }
+
+        percorrerArvore(no.esquerda);
+        percorrerArvore(no.direita);
+    }
+}
+
+class BFS {
+    public static void percorrerArvore(No raiz) {
+        if (raiz == null) {
+            return;
+        }
+
+        Queue<No> fila = new LinkedList<>();
+        fila.add(raiz);
+
+        while (!fila.isEmpty()) {
+            No no = fila.poll();
+
+            if (no.esquerda != null) {
+                fila.add(no.esquerda);
+            }
+            if (no.direita != null) {
+                fila.add(no.direita);
+            }
+        }
+    }
+}
+
 public class Main {
 
     public static void imprimir(No no) {
@@ -8,8 +53,8 @@ public class Main {
         }
 
         System.out.println(no.livro.getTitulo());
-        imprimir(no.filhoEsquerdo);
-        imprimir(no.filhoDireito);
+        imprimir(no.esquerda);
+        imprimir(no.direita);
     }
 
     public static void main(String[] args) {
@@ -18,8 +63,8 @@ public class Main {
         Livro livro3 = new Livro("O Pequeno Príncipe", "Antoine de Saint-Exupéry", 1943);
 
         No raiz = new No(livro1);
-        raiz.filhoEsquerdo = new No(livro2);
-        raiz.filhoDireito = new No(livro3);
+        raiz.esquerda = new No(livro2);
+        raiz.direita = new No(livro3);
 
         imprimir(raiz);
 
